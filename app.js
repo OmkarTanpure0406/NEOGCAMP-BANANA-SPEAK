2d97e3d1e0fe49876e3469e8e70aa0c3d864a441
Omkar Tanpure <omkartanpure999@gmail.com>
2022-05-23 11:21:17 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
 
 var txtinput = document.querySelector("#txt-input");
-console.log(txtinput)
+var outputDiv = document.querySelector("#output");
+
+
 
 function clickEventHandler() {
-    console.log("clicked");
-    console.log("input",txtinput.value);
+    outputDiv.innerText = "translate: " + txtinput.value
 }
 
 btnTranslate.addEventListener("click", clickEventHandler)
